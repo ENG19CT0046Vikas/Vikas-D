a8a5e89c62a789f3b5771a494b8ec9ec5da12783
ENG19CT0046Vikas <75782605+ENG19CT0046Vikas@users.noreply.github.com>
2021-03-31 19:33:57 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,1 +1,44 @@
 //WAP to find the distance between two points using structures and 4 functions.
+#include<stdio.h>
+#include<math.h>
+
+struct point
+{
+  float x1,x2,y1,y2;
+}
+a,b;
+ 
+float output(float first point,float second point)
+{
+  float distance;
+  distance = sqrt(firstpoint*firstpoint + secondpoint*secondpoint);
+  return distance;
+}
+  
+int main()
+{
+float first point,second point,Distance;
+printf("For the first point:\n");
+printf("Enter the value for X:\n");
+scanf("%f",&v.x1);
+printf("Enter the value for Y:\n");
+scanf("%f",&v.y2);
+  
+printf("For the second point:\n");
+printf("Enter the value For X;\n");
+scanf("%f",&j.x2);
+printf("Enter the value For Y:\n");
+scanf("%f",&j.y2);
+
+firstpoint = j.x2 - a.x1;
+secondpoint = j.y2 - a.y1;
+  
+distance = Output(firstpoint,secondpoint);
+  
+printf("the distance between the two points %f and %f: %f\n",firstpoint,secondpoint,Distance);
+  
+  return 0;
+}
+  
+
+
