0be9fefe6c92079bda8bf5076dd7d819a8c78d95
ENG19CT0046Vikas <75782605+ENG19CT0046Vikas@users.noreply.github.com>
2021-04-01 21:47:19 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -12,3 +12,24 @@ for(int i = 0;i<size;i++)
 {
 addition = addition + array[i];
 }
+display(addition);
+}
+int input()
+{
+    int array[21],size;
+    
+    
+    printf("Enter the size of numbers you want to add:\n");
+    scanf("%d",&size);
+    
+    printf("Enter %d elementes to add:\n",size);
+    for(int i = 0;i<size;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+    compute(array,size);
+}
+int main()
+{
+    input();
+}
